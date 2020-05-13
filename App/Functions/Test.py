4edabbb1d896cc302eed5d6d436517415a68397b
Mailine <nguyen.mailine@gmail.com>
2020-05-13 15:55:42 +0200

    Ajout_test

--- App/Functions/Test.py
+++ App/Functions/Test.py
@@ -1,3 +1,15 @@
-import json
-with open("DataTreatment\country.json") as json_file:
-    data = json.load(json_file)
+import unittest
+
+from FonctionBD import is_number
+
+class MyTest(unittest.TestCase):
+    def test_number(self):
+        self.assertTrue(is_number(5))
+        self.assertTrue(is_number("3643483"))
+        self.assertTrue(is_number(5.530))
+        self.assertFalse(is_number("5,530"))
+        self.assertFalse(is_number("treize"))
+
+
+if __name__ == '__main__':
+    unittest.main()
